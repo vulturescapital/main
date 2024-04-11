729f3ec73bc0a98158e0090e4d84da16a00c46ab
hlemonnier <hugo.lemonnier01@gmail.com>
2024-04-11 02:49:28 +0200

    new css

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Vultures Blog</title>
 <link rel="stylesheet" href="../css/styles.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
 <header>
     <div class="container header-content">
-        <h1>Vultures</h1>
+        <h1><a href="index.php" class="header-logo">Vultures</a></h1>
         <nav class="main-nav">
             <ul>
                 <li><a href="#">Blog</a></li>
@@ -19,7 +20,9 @@
             </ul>
         </nav>
         <div class="social-media">
-            <a href="#">Twitter</a>
+            <a href="twitter.com">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
         </div>
     </div>
 </header>
