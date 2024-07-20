0ae91d61982156092416cbc9d6173b741f3e0c14
hlemonnier <hugo.lemonnier01@gmail.com>
2024-07-20 18:46:22 +0200

    newsletter

--- newsletter.php
+++ newsletter.php
@@ -1,17 +1,15 @@
 <?php
-include 'dbconfig.php';
 include 'header.php';
 ?>
-<div class="newsletter-outer">
-    <h1 class="newsletter-title-page">NEWSLETTER</h1>
+<div class="container">
     <div class="newsletter-container">
         <div class="newsletter-header">JOIN OUR MAILING LIST</div>
         <div class="newsletter-title">
             Get the best stories from<br>
-            the Sequoia community.
+            the Vulture community.
         </div>
         <form class="newsletter-form" action="add_email_process.php" method="post">
-            <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
             <input type="email" name="email" placeholder="Email Address" class="newsletter-input" required>
             <button type="submit" class="newsletter-submit">Submit</button>
         </form>
